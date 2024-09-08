03cd476d9db993c5a7d47dd4c494ee26e95fd127
josuedelatorre35 <josuedelatorre35@yahoo.com>
2024-09-07 17:43:03 -0700

    added output msgs

--- Main.java
+++ Main.java
@@ -4,6 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
 
+        //Output message before taking input
+        System.out.println("Please enter information for the following fields:");
+        System.out.println("Artist Name, Birth Year, Death Year, Artwork Title, Artwork Year Published");
+        System.out.println("After entering information for one field, hit \"Enter\" to go to next field.");
+        System.out.println("For Birth/Death years unknown, type \"-1\". ");
+
         // Read input
         String artistName = scnr.nextLine();
         int birthYear = scnr.nextInt();
